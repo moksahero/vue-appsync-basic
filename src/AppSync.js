847dc858c2975b52acc3847840d87d9d282bf76f
Takeshi Amano <moksahero@gmail.com>
2018-07-07 15:08:27 +0900

    removed real keys in AppSync.js

---
+++ src/AppSync.js
@@ -0,0 +1,6 @@
+﻿export default {
+    "graphqlEndpoint": "__REPLACE_WITH_ENDPOINT__",
+    "region": "__REPLACE_WITH_REGION__",
+    "authenticationType": "API_KEY",
+    "apiKey": "__REPLACE_WITH_APIKEY__"
+}
